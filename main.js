0f7c85e8af559b9d62c201a81743ef85b0cc4ad8
Shivam Gupta <shivamgupta02005@gmail.com>
2023-11-11 23:39:45 +0530

    Create Gameboard factory which initialises and prints board to console

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-function gameBoard(){
-    board = [];
-    return {};
-}
-
-function Player(){
-    return {};
-}
-
-const Game = (function (){
-    return {};
+const Gameboard = (function(){
+    let board = ['','','','','','','','',''];
+    const printBoard = function(){
+        console.log(board[0],' | ',board[1],' | ',board[2],' | ',);
+        console.log(board[3],' | ',board[4],' | ',board[5],' | ',);
+        console.log(board[6],' | ',board[7],' | ',board[8],' | ',);
+    }
+    return {printBoard};
 })();
